219c4a62c289ef98266553d75bc7c32542840da3
hamzase <elamranih10@gmail.com>
2023-05-12 19:00:06 +0100

    Sceduler css event

--- src/components/Scheduler/Scheduler.js
+++ src/components/Scheduler/Scheduler.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import 'dhtmlx-scheduler';
 import 'dhtmlx-scheduler/codebase/dhtmlxscheduler_material.css';
 import axios from 'axios';
+import './Scheduler.css';
+
 
 const scheduler = window.scheduler;
 
@@ -17,7 +19,14 @@ export default class Scheduler extends Component {
     scheduler.attachEvent('onEventAdded', (id, ev) => {
       if (onDataUpdated) {
         onDataUpdated('create', ev, id);
+        
       }
+
+      scheduler.attachEvent('onEventCreated', function(id, e){
+        var input = e.getElementsByTagName('input')[0];
+        input.style.color = 'red'; // Change the color of the input element
+      });
+      
 
       // Save the new event to the backend
       axios.post('http://localhost:8082/Event/save', ev)
@@ -75,17 +84,42 @@ export default class Scheduler extends Component {
       'next'
     ];
     scheduler.config.hour_date = '%g:%i %A';
-    scheduler.xy.scale_width = 70;
+    scheduler.config.time_step = 30; // set
+  
+    // set the width of the time scale column
+    scheduler.xy.scale_width = 100;
+  
+    // set the height of the time scale labels
+    scheduler.config.hour_size_px = 88;
 
+    
+    // // change color of event 
+    // scheduler.templates.event_class = function(start, end, event) {
+    //   return 'my-event-class';
+    // };
+      //change color the tect of event
+    // scheduler.templates.event_text = function(start, end, event) {
+    //   return "<span style='color:black'>" + event.text + "</span>";
+    // };
+    
+
+    
+
+    // set the height of each event row
+
+  
     this.initSchedulerEvents();
-
+    
+  
     const { events } = this.props;
     scheduler.init(this.schedulerContainer, new Date());
     scheduler.clearAll();
     scheduler.parse(events);
     this.fetchEvents();
   }
-
+  
+  
+  
   fetchEvents = async () => {
     const response = await fetch('http://localhost:8082/Event');
     const events = await response.json();
@@ -111,7 +145,7 @@ export default class Scheduler extends Component {
     return (
       <div
         ref={(input) => { this.schedulerContainer = input }}
-        style={{ width: '100%', height: '100%' }}
+        style={{ width: '100%', height: '106%' }}
       ></div>
     );
   }
